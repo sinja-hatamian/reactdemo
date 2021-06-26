151f35870e541fb39d9f3a11db06a230bf95dd3d
Sina Hatamian <sina.hatamian75@gmail.com>
2021-06-26 16:13:45 +0430

    todo start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state =
+    {
+      username: "sina",
+      todoItems: [{ action: "do homework", done: true },
+      { action: "go to trip", done: true },
+      { action: "buy gift", done: false },
+      { action: "make a cake", done: true },
+      ],
+      newItemText: ""
+    }
+  }
+
+  updateNewTextValue = (event) => {
+    this.setState({ newItemText: event.target.value });
+  }
+  createNewTodo = () => {
+    if (!this.state.todoItems.find(item => item.action === this.state.newItemText)) {
+      this.setState({
+        todoItems: [...this.state.todoItems,
+        {
+          action: this.state.newItemText,
+          done: false
+        }],
+        newItemText: ""
+      })
+    }
+  }
+
+
+  render() {
+    return (
+      <div className="bg-success text-white text-center">
+        <h4>
+          {this.state.username}'s ToDo list
+          ({this.state.todoItems.filter(t=> !t.done).length}) items to do
+          <div className="container-fludi">
+            <div className="m-1">
+              <input className="form-control"
+                value={this.state.newItemText}
+                onChange={this.updateNewTextValue} />
+              <button className="btn btn-danger mt-1" onClick={this.createNewTodo}>
+                Add new item
+              </button>
+            </div>
+          </div>
+        </h4>
+      </div>
+    )
+  }
 }
-
-export default App;
